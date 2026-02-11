b082e8a78425c6952f55bbd988680460e8671a87
Yasindu Navodh <cst21024@std.uwu.ac.lk>
2026-02-11 23:01:07 +0530

    Create the admin login  controller function

---
+++ controllers/adminController.js
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken'
+
+
+export const adminLogin = async (req,res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (email !== process.env.ADMIN_Username || password !== process.env.ADMIN_Password) {
+            return res.json({ success: false, message: "Invalid Credentials" });
+        }
+        const token = jwt.sign({ email }, process.env.JWT_SECRET) 
+       return res.json({success: true,token})
+    } catch (error) {
+      return  res.json({ success: false, message: error.message });
+    }
+}
